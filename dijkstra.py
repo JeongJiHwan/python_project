47dd02fa2b69015898005ae527ff537fb9b35f10
JeongJiHwan <wlghks0461@gmail.com>
2020-08-13 16:37:46 +0900

    dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -1,32 +1,36 @@
-import heapq
-
-def dijkstra(G, s):
-    hq = []
+def dijkstra(G, dist, s):
+    queue = []
     for i in range(len(G)):
         tmp = [2100000000]
         dist[i] = tmp
-        hq.append((tmp, i))
-    dist[s][0]=0
-    while hq:
-        dis, u = heapq.heappop(hq)
+        queue.append((tmp,i))
+    dist[s][0] = 0
+    queue.sort(key = lambda e : e[0][0])
 
-        for v in G[u]:
-            if dist[v[0]][0] > dis[0] + v[1]:
-                dist[v[0]][0] = dis[0] + v[1]
-                
-T = int(input())
-for _ in range(T):
-    N,M = map(int, input().split())
-    List = [[] for x in range(N)]
-    
-    for i in range(M):
-        u,v,c = map(int, input().split())
-        List[u].append((v,c))
+    while queue:
+        d, u = queue.pop(0)
+
+        for v,s in G[u]:
+            if dist[v][0] > d[0] + s:
+                dist[v][0] = d[0] + s
         
-    dist = [0 for i in range(N)]
-    dijkstra(List, 0)
+        queue.sort(key = lambda e : e[0][0])
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+
+    ls = [[] for i in range(N)]
+
+    for i in range(M):
+        u, v, c = map(int, input().split())
+        ls[u].append((v,c))
     
-    if dist[N-1][0]==2100000000:
+    dist = [0 for i in range(N)]
+
+    dijkstra(ls, dist, 0)
+
+    if dist[N-1][0] == 2100000000:
         print(-1)
     else:
         print(dist[N-1][0])
